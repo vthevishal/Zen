282cd9ec7e476b7a84c9d512647cea9d5edd9e04
V VISHAL <60169794+vthevishal@users.noreply.github.com>
2020-04-01 17:26:19 +0530

    Create datatype.js

---
+++ datatype.js
@@ -0,0 +1,17 @@
+var a=5;
+let a1=6
+const a2=4
+b=5.0
+c=true
+var d="lets rock"
+var e = {
+    firstName:"vishal", lastName:"v"};  
+var f = [
+    {firstName:"vishal", lastName:"v"}, 
+    {firstName:"karan", lastName:"vj"}];    
+alert(a+"is"+typeof(a))
+alert(b+" is " +typeof(b))
+alert(c+" is " +typeof(c))
+alert(d+" is " +typeof(d))
+alert(e+" is " +typeof(e))
+alert(f+" is " +typeof(f))
